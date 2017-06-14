8c00bbb29458b02054f1187e051afafdfbc624c4
Aeternia <gherasymuk@gmail.com>
2017-06-14 21:57:11 +0300

    Geojson formatting

---
+++ twentythirteenstudentsdata.js
@@ -0,0 +1,216 @@
+var twentythirteenstudentsdata = {
+	"type": "FeatureCollection",
+	"features": [
+{
+        "type": "Feature",
+        "properties": {
+            "name": "Вікторія Гайбонюк",
+            "city": "Рівне",
+            "show_on_map": true,
+            "previous_uni": [35.139567, 47.838800]
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [26.251617, 50.619900]
+        }
+    }, 
+	{
+        "type": "Feature",
+        "properties": {
+            "name": "Анастасія Онопрійчук",
+            "city": "Рівне",
+            "show_on_map": true,
+            "previous_uni": [35.139567, 47.838800]
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [26.251617, 50.619900]
+        }
+    },
+	{
+        "type": "Feature",
+        "properties": {
+            "name": "Карина Лазарук",
+            "city": "Київ",
+            "show_on_map": true,
+            "previous_uni": [50.450100, 30.523400]
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [26.251617, 50.619900]
+        }
+    },
+	{
+        "type": "Feature",
+        "properties": {
+            "name": "Юрій Кульчинський",
+            "city": "Рівне",
+            "show_on_map": true,
+            "previous_uni": [35.139567, 47.838800]
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [26.251617, 50.619900]
+        }
+    },
+	{
+        "type": "Feature",
+        "properties": {
+            "name": "Галина Герасим",
+            "city": "Львів",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [24.029717, 49.839683]
+        }
+    }, 
+	
+	{
+        "type": "Feature",
+        "properties": {
+            "name": "Надія Михалевич",
+            "city": "Львів",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [24.029717, 49.839683]
+        }
+    },
+	{
+        "type": "Feature",
+        "properties": {
+            "name": "Мар’яна Мазурак",
+            "city": "Львів",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [24.029717, 49.839683]
+        }
+    },
+	{
+        "type": "Feature",
+        "properties": {
+            "name": "Яна Машкова",
+            "city": "Корсунь-Шевченківський",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [31.261853, 49.426112]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "name": "Олеся Малюванчук",
+            "city": "Івано-Франківськ",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [24.711117, 48.922633]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Софія Кочмар",
+            "city": "Бузьк",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [24.609461, 49.969359]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Вікторія Ільченко",
+            "city": "Випасне",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [30.266619, 46.196812]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Ігор Фещенко",
+            "city": "Сміла",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [31.852233, 49.227717]
+        }
+
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Марта Дацюк",
+            "city": "Сокаль",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [24.280156, 50.486354]
+        }
+
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Богдана Рущак",
+            "city": "Буштино",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [23.482774, 48.049668]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Анна Казак",
+            "city": "Запоріжжя",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [35.139567, 47.838800]
+        }
+    },
+	{
+        "type": "Feature",
+        "properties": {
+            "name": "Джессіка Пачеко-Семенюк",
+            "city": "Savannah",
+            "show_on_map": true
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [32.110746, -81.191454]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Каріна Фурса",
+            "city": "Богданівка",
+            "show_on_map": true,
+            "previous_uni": [35.139567, 47.838800]
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [36.046944, 47.105833]
+        }
+    }
+]
+};
